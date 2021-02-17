e3df52f56a48ba067d647659d0fbb1c84a1065ef
jgrove4 <77176628+jgrove4@users.noreply.github.com>
2021-02-17 18:03:02 +0000

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -43,3 +43,11 @@ names(Data)<-gsub("Acc", "Accelerometer", names(Data))
 names(Data)<-gsub("Gyro", "Gyroscope", names(Data))
 names(Data)<-gsub("Mag", "Magnitude", names(Data))
 names(Data)<-gsub("BodyBody", "Body", names(Data))
+
+#Creating a second tidy dataset with the average of each variable for each activity and each subject
+
+library(plyr);
+Data2<-aggregate(. ~subject + activity, Data, mean)
+Data2<-Data2[order(Data2$subject,Data2$activity),]
+write.table(Data2, file = "tidydata.txt",row.name=FALSE)
+
